2185433ddc87ea72b73fab892b1d1be4a3fceb29
TakakiNishio <p104314t@mail.kyutech.jp>
2016-07-29 00:36:08 +0900

    N

---
+++ k_nearest_neighbors/k_nearest_average.py
@@ -0,0 +1,110 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+#define function
+def f(x1,x2,d):
+    alpha = 2
+    beta  = 5
+    gamma = 3
+    ans = alpha + beta * x1 + gamma * x2 + np.random.rand() * d
+    return ans
+
+
+#create data
+def data_generator(data_n,d):
+    l = 7
+    raw_data = []
+    plt.figure(1)
+    for i in range(0,data_n):
+        x1 = np.random.rand()
+        x2 = np.random.rand()
+        h = f(x1,x2,d)
+        if h > l:
+            y = 1
+            plt.plot(x1,x2,"bo")
+        else:
+            y = 0
+            plt.plot(x1,x2,"ro")
+
+        raw_data.append([[x1,x2],y])
+    return raw_data
+
+
+#estimation (k-neighbors)
+def estimation(training_set,test_set,k):
+
+    y = []
+    yh = []
+
+    for i in range(0,len(test_set)):
+        dist_list = []
+        min_dist = 1000
+        sum_y = 0
+        for j in range(0,len(training_set)):
+            dist = math.sqrt(((test_set[i][0][0] - training_set[j][0][0])**2) +((test_set[i][0][1] - training_set[j][0][1])**2))
+            dist_list.append([dist,j])
+
+        dist_list = sorted(dist_list)
+
+        for n in range(0,k):
+            index_y = dist_list[n][1]
+            sum_y = sum_y + training_set[index_y][1]
+
+        if sum_y > int(k/2)+1:
+            yh.append(1)
+        else:
+            yh.append(0)
+
+    #success_counter
+    success = 0
+    for i in range(0,test_n):
+        if test_set[i][1] == yh[i]:
+            success = success + 1
+
+    error_rate = (1.0 - float(success)/test_n) * 100
+
+    return error_rate
+
+
+#main function
+if __name__ == '__main__':
+
+    N = 2
+    data_n = 2000
+    train_n = 1800
+    test_n = 200
+    k_max = 10
+    disturbance = 2
+
+    data = []
+    training_set = []
+    test_set = []
+
+    k_list = []
+    error_list = []
+
+    data = data_generator(data_n,disturbance)
+
+    for k in range(5,k_max+1):
+
+        k_list.append(k)
+        error_sum = 0
+
+        for i in range(0,N-1):
+
+            for j in range(0,train_n):
+                training_set.append(data[i])
+
+            for j in range(0,test_n):
+                test_set.append(data[i+train_n])
+
+            error_sum = error_sum + estimation(training_set,test_set,k)
+
+        error_list.append(error_sum/N)
+        print "k : "+ str(k) + "  error_average : " + str(error_sum/5) + "[%]"
+
+    plt.figure(2)
+    plt.plot(k_list,error_list,"ro-")
+    plt.show()
