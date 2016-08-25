39bcefb54203911044657a361c3a064747045626
TakakiNishio <p104314t@mail.kyutech.jp>
2016-08-25 17:47:08 +0900

    0825

--- linear_regression/rss_v3.py
+++ linear_regression/rss_v3.py
@@ -24,32 +24,40 @@ def data_generator(data_n):
 #calculate RSS
 def rss(data,beta_n):
     r_n = 0
-    for i in range(len(data)):
+    for i in range(0,len(data)):
         r_n = r_n + (data[i][1] - (data[i][0] * beta_n[1] + beta_n[0]))**2
+    r_n/=float(len(data))
     return r_n
 
 
 #linear_regression -- random search
 def random_search(data):
     first_beta = [1,1]
-    last_beta = []
+    last_beta = [1,1]
     beta_n = [1,1]
     min_r_n = rss(data,first_beta)
-    for i in range(0,10000):
+    plt.figure(2)
+    plt.plot(1.4,3.2,"ro")
+    for i in range(0,100000):
         beta_n[0] = 10 * np.random.rand()
         beta_n[1] = 10 * np.random.rand()
         r_n = rss(data,beta_n)
         if r_n < min_r_n :
             min_r_n = r_n
-            last_beta = beta_n
+            last_beta[0] = beta_n[0]
+            last_beta[1] = beta_n[1]
+            print "beta0 : "+ str(last_beta[0]) + "  beta1 : "+ str(last_beta[1]) + "  RSS : "+ str(min_r_n)
+            plt.figure(2)
+            plt.plot(last_beta[0],last_beta[1],"bo")
+    plt.plot(last_beta[0],last_beta[1],"go")
     return last_beta
 
 
-#confirm the result
+#confirm the result of estimation
 def h(n,last_beta):
     h = []
     for i in range(0,n):
-        y = last_beta[0] * i + last_beta[1]
+        y = last_beta[1] * i + last_beta[0]
         h.append(y)
         plt.figure(1)
         plt.plot(i,y,"bo")
@@ -59,7 +67,7 @@ def h(n,last_beta):
 #main function
 if __name__ == '__main__':
 
-    n = 20
+    n = 100
     dataset = data_generator(n)
     searched_beta = random_search(dataset)
     result_data = h(n,searched_beta)
